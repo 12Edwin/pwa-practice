35a1b48dcd563197633d4e50f25e4109c0f83827
Edwin <20213tn046@utez.edu.mx>
2024-09-17 09:48:19 -0600

    [ADD] all

---
+++ app.js
@@ -0,0 +1,10 @@
+// Aquí puedes agregar la lógica principal de tu aplicación
+console.log('La aplicación PWA está funcionando');
+
+// Ejemplo de función
+function saludar(nombre) {
+    alert('Hola, ' + nombre + '! Bienvenido a nuestra PWA.');
+}
+
+// Puedes llamar a esta función desde tu HTML o aquí mismo
+// saludar('Usuario');
